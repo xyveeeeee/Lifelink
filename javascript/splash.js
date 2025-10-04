1d0f400c86ca48fc4d7293d855b802bdab35e174
xyveeeeee <xyance26@gmail.com>
2025-10-05 00:55:51 +0800

    modified:   css/splash.css 	modified:   html/splash.html 	modified:   javascript/splash.js

--- javascript/splash.js
+++ javascript/splash.js
@@ -1,12 +1,5 @@
-const splashDuration = 5000; 
+const splashDuration = 8000; 
 setTimeout(() => {
-  // Option 1: Redirect to another page
   window.location.href = "../html/Log in.html"; 
 
-  const splash = document.querySelector(".splash");
-  splash.style.opacity = "0";
-  setTimeout(() => {
-    splash.style.display = "none";
-    document.querySelector(".main").style.display = "block";
-  }, 1000);
 }, splashDuration);
